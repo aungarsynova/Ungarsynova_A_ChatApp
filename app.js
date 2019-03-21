7c98725617d3be110524a7ce1baafe4f78730658
unknown <you@example.com>
2019-03-21 17:29:19 -0400

    chat app start

---
+++ app.js
@@ -0,0 +1,20 @@
+//initialize an express app and set it up
+const express = require('express');
+const app = express();
+const io = require('socket.io')();
+
+//some config stuff
+const port = process.env.PORT || 3000;
+
+//tell our app to use the public folder for static files
+app.use(express.static('public'));
+
+//instansiate the only route we need
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+//create server variable for socket io
+const server = app.listen(port, () => {
+    console.log(`app is running in the port ${port}`)
+});
